9fe65978e8afc1b2f098d6ab24658b637bd5734d
RogerMontalvo <rogermontalvo7@gmail.com>
2017-02-22 16:40:53 +0100

    4.2

--- _4.1/_4.1.ino
+++ _4.1/_4.1.ino
@@ -21,10 +21,15 @@ void setup() {                            // CONDICIONS INICIALS
   Serial.begin(9600);     // set up Serial library at 9600 bps
 
   if ( tempAigua > 100)
-  {
+{
     Serial.print("Aigua supera els 100C, esta bullint!");
-  } 
 }
+  else if ( tempAigua < 100 )
+{
+  Serial.print("Aigua encara no bull");
+}
+} 
+
 
 
 
