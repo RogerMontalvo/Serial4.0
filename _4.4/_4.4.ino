bda5cc3c9ac985a43095af051e2a5746eda7ddb7
RogerMontalvo <rogermontalvo7@gmail.com>
2017-03-01 19:58:01 +0100

    Right

--- _4.4/_4.4.ino
+++ _4.4/_4.4.ino
@@ -20,94 +20,96 @@ void setup()
 {                    // CONDICIONS INICIALS
 
 
-  if ( 0 < KgCO2_m2  <3.5 ){
-Nivell == 0 ;
+  if (( 0 < KgCO2_m2 )&& (KgCO2_m2 < 3.5))
+  {
+Nivell = 0 ;
   }
-  else if ( 3.5 <= KgCO2_m2  < 6.5){
-Nivell == 1 ;
-}
-  else if ( 6.5 <= KgCO2_m2  < 11.1){
-Nivell == 2 ;
-}
-  else if ( 11.1 <= KgCO2_m2  < 17.7){
-Nivell == 3 ;
+  else if ((3.5 <= KgCO2_m2) && (KgCO2_m2 < 6.5))  
+  {
+Nivell= 1 ;
   }
-  else if ( 17.7 <= KgCO2_m2  < 38.2){
-Nivell == 4 ;
+  else if ((6.5 <= KgCO2_m2) && (KgCO2_m2 < 11.1))
+  {
+Nivell= 2 ;
   }
-  else if ( 38.2 <= KgCO2_m2  < 43.2){
-Nivell == 5 ;
+  else if ((11.1 <= KgCO2_m2) && (KgCO2_m2 < 17.7))
+  {
+Nivell= 3 ;
   }
-  else if ( 43.2 <= KgCO2_m2){
-Nivell == 6 ;
+  else if ((17.7 <= KgCO2_m2) && (KgCO2_m2 < 38.2))
+  {
+Nivell= 4 ;
+  }
+  else if ((38.2 <= KgCO2_m2) && (KgCO2_m2 < 43.2))
+  {
+Nivell= 5 ;
+  }
+  else if (43.2 <= KgCO2_m2)
+  {
+Nivell= 6 ;
   }  
+  else 
+  {
+Nivell= 7 ;
+  }
 
+///////
   Serial.begin(9600);     // Set up Serial library at 9600 bps
 
-  switch ( Nivell )
+  Serial.print ("El nivell de contaminacio es:");
+  switch (Nivell)
 {  
   case 1:
   {
-  Serial.print ("A") ;
+  Serial.println ("A") ;
+  break ;
   }
-  break;
-
+  
   case 2:
-  Serial.print ("B") ;
+  {
+  Serial.println ("B") ;
   break;
+  }
   
   case 3:
-  Serial.print ("C") ;
+  {
+  Serial.println ("C") ;
   break;
+  }
   
   case 4:
-  Serial.print ("D") ;
+  {
+  Serial.println ("D") ;
   break;
+  }
   
   case 5:
-  Serial.print ("E") ;
+  Serial.println ("E") ;
   break;
   
   case 6:
-  Serial.print ("F") ;
+  {
+  Serial.println ("F") ;
   break;
+  }
   
   case 7:
-  Serial.print ("G") ;
+  {
+  Serial.println ("G") ;
   break;
+  }
 
+  default:    // Cas impossible (Valor negatiu KGCO2_M2)
+  {
+  Serial.println ("Sensor Avariat");
+  break;
+  }
 }
-
-
- 
-
-      
-  
- 
-
-  
-
-
-
-
-
-  
-
-
-
-
-
-
-     
-
-     
-
 }
 
 
 
-
-
+ 
 //**************************** LOOP *************************************
 
 void loop()            //BUCLE
@@ -115,3 +117,5 @@ void loop()            //BUCLE
 
 }
 //*************************** FUNCIONS **********************************
+  
+
